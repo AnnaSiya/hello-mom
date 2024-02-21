66c3aeec64e9dfa1ac37ab246490da59ec32b0d8
AnnaSiya <160726140+AnnaSiya@users.noreply.github.com>
2024-02-21 16:03:01 +0600

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function () {
+  output.innerHTML = 'Мама, я тебя люблю💋💋💋💕💕❤❤';
+});
